bf2f0db3d2822ded51ad560d39df9091851e701d
Eliseu <eliseutorres.gx@gmail.com>
2018-03-14 14:21:56 -0300

    Update topo.py

--- topo.py
+++ topo.py
@@ -37,9 +37,7 @@ def topology():
 	print "*** Creating nodes"
 	h1 = net.addHost( 'h1', ip='10.0.0.1/8')
 	h2 = net.addHost( 'h2', ip='10.0.0.2/8')
-	h3 = net.addHost( 'h3', ip='10.0.0.3/8')
-	h4 = net.addHost( 'h4', ip='10.0.0.4/8')
-	h5 = net.addHost( 'h5', ip='10.0.0.5/8')
+	h3 = net.addHost( 'h3', ip='10.0.0.3/8')	
 	h6 = net.addHost( 'h6', ip='10.0.0.6/8')
 	
 	
@@ -48,15 +46,13 @@ def topology():
 	s3 = net.addSwitch('s3')
 	
 	c1 = net.addController( 'c1', controller=RemoteController, ip='127.0.0.1', port=6633)
-	#c1 = net.addController('c1', controller=Controller)
+	
 
 	print "*** Associating and Creating links"
 	net.addLink(h1, s1)
 	net.addLink(h2, s1)
 	net.addLink(h3, s1)
-	net.addLink(s1, s2)
-	net.addLink(s2, h4)
-	net.addLink(h5, s3)
+	net.addLink(s1, s2)	
 	net.addLink(h6, s3)
 	net.addLink(s3, s2)
 	
