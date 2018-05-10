dd66b243cd18f11a5d18dd54a0b02f260f280bb4
Eliseu <eliseutorres.gx@gmail.com>
2018-05-09 22:53:26 -0300

    Update topo.py

--- topo.py
+++ topo.py
@@ -37,7 +37,9 @@ def topology():
 	print "*** Creating nodes"
 	h1 = net.addHost( 'h1', ip='10.0.0.1/8')
 	h2 = net.addHost( 'h2', ip='10.0.0.2/8')
-	h3 = net.addHost( 'h3', ip='10.0.0.3/8')	
+	h3 = net.addHost( 'h3', ip='10.0.0.3/8')
+	h4 = net.addHost( 'h4', ip='10.0.0.4/8')
+	h5 = net.addHost( 'h5', ip='10.0.0.5/8')	
 	h6 = net.addHost( 'h6', ip='10.0.0.6/8')
 	
 	
@@ -52,7 +54,8 @@ def topology():
 	net.addLink(h1, s1)
 	net.addLink(h2, s1)
 	net.addLink(h3, s1)
-	net.addLink(s1, s2)	
+	net.addLink(s1, s2)
+	net.addLink(h4, s2)	
 	net.addLink(h6, s3)
 	net.addLink(s3, s2)
 	
