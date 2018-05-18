3e46b664c4e872758e38c90cb3c92c4a8b3df069
Eliseu <eliseutorres.gx@gmail.com>
2018-05-18 13:06:44 -0300

    Update topo.py

--- topo.py
+++ topo.py
@@ -39,7 +39,7 @@ def topology():
 	h2 = net.addHost( 'h2', ip='10.0.0.2/8')
 	h3 = net.addHost( 'h3', ip='10.0.0.3/8')
 	h4 = net.addHost( 'h4', ip='10.0.0.4/8')
-	h5 = net.addHost( 'h5', ip='10.0.0.5/8')	
+	h5 = net.addHost( 'h5', ip='10.0.0.5/8')
 	h6 = net.addHost( 'h6', ip='10.0.0.6/8')
 	
 	
@@ -55,7 +55,8 @@ def topology():
 	net.addLink(h2, s1)
 	net.addLink(h3, s1)
 	net.addLink(s1, s2)
-	net.addLink(h4, s2)	
+	net.addLink(s2, h4)
+	net.addLink(h5, s3)
 	net.addLink(h6, s3)
 	net.addLink(s3, s2)
 	
